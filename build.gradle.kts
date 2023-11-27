6f3e9640aa8acbd9e445751345111a37f094aac2
Zaemon <79342128+ZaemonGames@users.noreply.github.com>
2023-11-28 02:42:42 +0900

    [Change] 依存関係を整理

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,24 +11,48 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
-    maven ("https://papermc.io/repo/repository/maven-public/")
-    maven ("https://oss.sonatype.org/content/groups/public/")
-    maven ("https://jitpack.io")
-    maven ("https://repo.codemc.io/repository/maven-snapshots/")
-    maven ("https://repo.opencollab.dev/main/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+
+    /*
+     * item  : PaperMC
+     * usage : Base
+     */
+    maven("https://papermc.io/repo/repository/maven-public/")
+
+
+    /*
+     * item  : Opencollab Repository
+     * usage : FloodgateApi
+     */
+    maven("https://repo.opencollab.dev/main/")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
 
+    /*
+     * item  : MySQL
+     * usage : DataStorage
+     */
     val exposedVersion = "0.39.2"
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("mysql:mysql-connector-java:8.0.33")
 
+    /*
+     * item  : DiscordWebhook
+     * usage : /report Transaction
+     */
     implementation("club.minnced", "discord-webhooks", "0.8.4")
-    implementation("io.ktor","ktor-server-core-jvm", "2.3.6")
+
+    /*
+     * item  : Ktor,    Thumbnailator, Klaxon
+     * usage : FaceApi, ImageResize  , JsonParser
+     */
+    implementation("io.ktor", "ktor-server-core-jvm", "2.3.6")
     implementation("io.ktor", "ktor-server-netty-jvm", "2.3.6")
     implementation("net.coobird", "thumbnailator", "0.4.14")
     implementation("com.beust", "klaxon", "5.5")
@@ -37,9 +61,22 @@ dependencies {
     implementation("com.github.M1n1don", "SmartInvsR", "2.0.0")
     implementation("com.github.hazae41", "mc-kutils", "master-SNAPSHOT")
 
+    /*
+     * item  : LuckPermsApi
+     * usage : /staffPermissionModify
+     */
     compileOnly("net.luckperms:api:5.4")
-    compileOnly("com.github.MilkBowl", "VaultAPI", "1.7")
+
+    /*
+     * item  : PaperMCApi
+     * usage : Base
+     */
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+
+    /*
+     * item  : FloodgateApi
+     * uasge : FaceApi
+     */
     compileOnly("org.geysermc.floodgate:api:2.2.2-SNAPSHOT")
 }
 
